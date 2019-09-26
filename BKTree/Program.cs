0e106c233dc6c62b5f7849d0c3eef02528fb28ee
biagianina <biagianina02@gmail.com>
2019-09-26 21:40:11 +0300

    Final

--- BKTree/Program.cs
+++ BKTree/Program.cs
@@ -6,6 +6,22 @@ namespace BKTree
     {
         static void Main()
         {
+            string[] dictionary =
+            {
+                "hell", "help", "shel", "smell",
+                "fell", "felt", "oops", "pop", "oouch", "halt"
+            };
+            CreateBKTree tree = new CreateBKTree();
+            for (int i = 0; i < dictionary.Length; i++)
+            {
+                tree.Add(dictionary[i]);
+            }
+
+            const int tolerance = 2;
+            foreach (var match in tree.Match("helt", tolerance))
+            {
+                Console.WriteLine(match);
+            }
         }
     }
 }
